a4a697b716576927750452eecfb6c62d8923fdc8
gyaru <eryla@eru.me>
2017-09-09 00:18:58 +0200

    fixes

--- js/tabber.js
+++ js/tabber.js
@@ -25,6 +25,21 @@
 				nav.find('a[title="' + title + '"]').parent().addClass('tabberactive');
 				return true;
 			}
+			function showContentScroll(title) {
+				// literally nearly identical function but with scroll, wow i'm so good at js
+				var title = title.replace("_", " "); // wiki doesn't like spaces in links
+				var title = title.replace(".2B", "+");
+				var title = title.replace(".28", "(");
+				var title = title.replace(".29", ")");
+				var content = tabContent.filter('[title="' + title + '"]');
+				if (content.length !== 1) return false;
+				tabContent.hide();
+				content.show();
+				nav.find('.tabberactive').removeClass('tabberactive');
+				nav.find('a[title="' + title + '"]').parent().addClass('tabberactive');
+				$('a[title="' + title + '"]')[0].scrollIntoView();
+				return true;
+			}
 			// setup initial state
 			var loc;
 			if (mw.config.get("wgInternalRedirectTargetUrl")) {
@@ -33,7 +48,7 @@
 			else {
 				loc = decodeURI(location.hash.replace('#', '').replace(/\.([0-9A-F]{2})/g, "%$1"));
 			}
-			if ( loc == '' || !showContent(loc) ) {
+			if ( loc == '' || !showContentScroll(loc) ) {
 				showContent(tabContent.first().attr('title'));
 			}
 
